64124ac359396127676ed90732fb2bd74c028f4d
Yingqing Shan <yingqing.shan@sayweee.com>
2022-11-20 00:31:47 +0800

    异常错误判断优化

--- apps/ext/exceptions/__init__.py
+++ apps/ext/exceptions/__init__.py
@@ -13,7 +13,6 @@
 """
 
 from fastapi import FastAPI, Request
-from apps.ext.response.json_response import *
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.exceptions import HTTPException as FastapiHTTPException
 from fastapi.exceptions import RequestValidationError
@@ -33,10 +32,11 @@ class ApiExceptionHandler:
             self.init_app(app)
 
     def init_app(self, app: FastAPI):
-
-        # @app.exception_handler(StarletteHTTPException)
-        # @app.exception_handler(RequestValidationError)
-        # @app.exception_handler(Exception)
+        """
+        初始化异常错误判断
+        :param app:
+        :return:
+        """
         app.add_exception_handler(Exception, handler=self.all_exception_handler)
         # 捕获StarletteHTTPException返回的错误异常，如返回405的异常的时候，走的是这个地方
         app.add_exception_handler(StarletteHTTPException, handler=self.http_exception_handler)
@@ -51,53 +51,17 @@ class ApiExceptionHandler:
             pass
         elif isinstance(exc.raw_errors[0].exc, MissingError):
             pass
-        return ParameterException(http_status_code=400, api_code=400, message='参数校验错误', result={
-            "detail": exc.errors(),
-            "body": exc.body
-        })
+        return ParameterException(data={"detail": exc.errors(), "body": exc.body })
 
     @staticmethod
-    async def all_exception_handler(request: Request, exc: Exception):
+    async def http_exception_handler(request: Request, exc: StarletteHTTPException):
         """
         全局的捕获抛出的HTTPException异常，注意这里需要使用StarletteHTTPException的才可以
         :param request:
         :param exc:
         :return:
         """
-        # log_msg = f"捕获到系统错误：请求路径:{request.url.path}\n错误信息：{traceback.format_exc()}"
-        if isinstance(exc, StarletteHTTPException) or isinstance(exc, FastapiHTTPException):
-            if exc.status_code == 405:
-                return MethodnotallowedException()
-            if exc.status_code == 404:
-                return NotfoundException()
-            elif exc.status_code == 429:
-                return LimiterResException()
-            elif exc.status_code == 500:
-                return InternalErrorException()
-            elif exc.status_code == 400:
-                # 有部分的地方直接的选择使用raise的方式抛出了异常，这里也需要进程处理
-                # raise HTTPException(HTTP_400_BAD_REQUEST, 'Invalid token')
-                return BadrequestException(msg=exc.detail)
-
-            return BadrequestException()
-        else:
-            # 其他内部的异常的错误拦截处理
-            logger.exception(exc)
-            traceback.print_exc()
-            return InternalErrorException()
-
-    @staticmethod
-    async def http_exception_handler(request: Request, exc: StarletteHTTPException):
-        '''
-           全局的捕获抛出的HTTPException异常，注意这里需要使用StarletteHTTPException的才可以
-           :param request:
-           :param exc:
-           :return:
-           '''
         # 这里全局监听了我们的所有的HTTP响应，包括了200 的也会尽到这里来！
-        # print("撒很好收到哈搜地和撒谎的撒22222222222===========",exc)
-        # log_msg = f"捕获到系统错误：请求路径:{request.url.path}\n错误信息：{traceback.format_exc()}"
-
         if exc.status_code == 405:
             return MethodnotallowedException()
         if exc.status_code == 404:
@@ -107,6 +71,21 @@ class ApiExceptionHandler:
         elif exc.status_code == 500:
             return InternalErrorException()
         elif exc.status_code == 400:
-            # 有部分的地方直接的选择使用raise的方式抛出了异常，这里也需要进程处理
-            # raise HTTPException(HTTP_400_BAD_REQUEST, 'Invalid token')
-            return BadrequestException(msg=exc.detail)
+            return BadrequestException(message=exc.detail)
+
+    @staticmethod
+    async def all_exception_handler(self, request: Request, exc: Exception):
+        """
+        全局的捕获抛出的HTTPException异常，注意这里需要使用StarletteHTTPException的才可以
+        :param self:
+        :param request:
+        :param exc:
+        :return:
+        """
+        if isinstance(exc, StarletteHTTPException) or isinstance(exc, FastapiHTTPException):
+            self.http_exception_handler(request, exc)
+        else:
+            # 其他内部的异常的错误拦截处理
+            logger.exception(exc)
+            traceback.print_exc()
+            return InternalErrorException()
