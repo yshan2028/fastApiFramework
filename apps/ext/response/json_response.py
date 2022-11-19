4277e5fea324c10b5e18a70bf8b52616a09992a0
六月 <462548187@qq.com>
2022-11-19 17:00:40 +0800

    优化 错误代码 修改 result 改为 data 字段

--- apps/ext/response/json_response.py
+++ apps/ext/response/json_response.py
@@ -24,17 +24,17 @@ class ApiResponse(JSONResponse):
     # 定义返回响应码--如果不指定的话则默认都是返回200
     http_status_code = 200
     # 默认成功
-    api_code = 0
+    api_code = 20000
     # 默认Node.如果是必选的，去掉默认值即可
-    result: Optional[Dict[str, Any]] = None  # 结果可以是{} 或 []
-    message = '成功'
+    data: Optional[Dict[str, Any]] = None  # 结果可以是{} 或 []
+    message = '请求成功'
     success = True
     timestamp = int(time.time() * 1000)
 
-    def __init__(self, success=None, http_status_code=None, api_code=None, result=None, message=None, **options):
+    def __init__(self, success=None, http_status_code=None, api_code=None, data=None, message=None, **options):
 
-        if result:
-            self.result = result
+        if data:
+            self.data = data
         if message:
             self.message = message
 
@@ -52,7 +52,7 @@ class ApiResponse(JSONResponse):
             message=self.message,
             code=self.api_code,
             success=self.success,
-            result=self.result,
+            data=self.data,
             timestamp=self.timestamp,
         )
 
@@ -61,123 +61,123 @@ class ApiResponse(JSONResponse):
 
 
 class BadrequestException(ApiResponse):
-    http_status_code = 400
-    api_code = 10031
-    result = None  # 结果可以是{} 或 []
+    http_status_code = 400  # 错误的请求
+    api_code = 20030
+    data = None  # 结果可以是{} 或 []
     message = '错误的请求'
     success = False
 
 
 class LimiterResException(ApiResponse):
-    http_status_code = 429
-    api_code = 429
-    result = None  # 结果可以是{} 或 []
+    http_status_code = 429  # 请求太多
+    api_code = 20032
+    data = None  # 结果可以是{} 或 []
     message = '访问的速度过快'
     success = False
 
 
 class ParameterException(ApiResponse):
-    http_status_code = 400
-    result = {}
+    http_status_code = 400  # 参数错误的请求
+    api_code = 20031  # 参数错误, 'Params error'
+    data = {}
     message = '参数校验错误,请检查提交的参数信息'
-    api_code = 10031
     success = False
 
 
 class UnauthorizedException(ApiResponse):
-    http_status_code = 401
-    result = {}
+    http_status_code = 401  # 未经授权
+    api_code = 20001  # 无效的token, 'Invalid token'
+    data = {}
     message = '未经许可授权'
-    api_code = 10032
     success = False
 
 
 class ForbiddenException(ApiResponse):
-    http_status_code = 403
-    result = {}
+    http_status_code = 403  # 禁止访问
+    api_code = 20033  # 访问地址失败
+    data = {}
     message = '失败！当前访问没有权限，或操作的数据没权限!'
-    api_code = 10033
     success = False
 
 
 class NotfoundException(ApiResponse):
-    http_status_code = 404
-    result = {}
+    http_status_code = 404  # 没找到
+    api_code = 20034  # 访问不存在
+    data = {}
     message = '访问地址不存在'
-    api_code = 10034
     success = False
 
 
 class MethodnotallowedException(ApiResponse):
-    http_status_code = 405
-    result = {}
+    http_status_code = 405  # 方法不允许
+    api_code = 20035
+    data = {}
     message = '不允许使用此方法提交访问'
-    api_code = 10034
     success = False
 
 
 class OtherException(ApiResponse):
-    http_status_code = 800
-    result = {}
+    http_status_code = 400  # 未知错误的请求
+    api_code = 99999
+    data = {}
     message = '未知的其他HTTPEOOER异常'
-    api_code = 10034
     success = False
 
 
 class InternalErrorException(ApiResponse):
     http_status_code = 500
-    result = {}
+    api_code = 20500
+    data = {}
     message = '程序员哥哥睡眠不足，系统崩溃了！'
-    api_code = 500
     success = False
 
 
 class InvalidTokenException(ApiResponse):
-    http_status_code = 401
-    api_code = 401
+    http_status_code = 401  # 未经授权
+    api_code = 20002  # 无效的token, 'Invalid token'
     message = '很久没操作，令牌失效'
     success = False
 
 
 class ExpiredTokenException(ApiResponse):
-    http_status_code = 422
+    http_status_code = 422  # 令牌过期
+    api_code = 20003  # access token过期, 'Access token expired'
     message = '很久没操作，令牌过期'
-    api_code = 10050
     success = False
 
 
 class FileTooLargeException(ApiResponse):
-    http_status_code = 413
-    api_code = 413
-    result = None  # 结果可以是{} 或 []
+    http_status_code = 413  # 请求包太大
+    api_code = 20023
+    data = None  # 结果可以是{} 或 []
     message = '文件体积过大'
 
 
 class FileTooManyException(ApiResponse):
     http_status_code = 413
     message = '文件数量过多'
-    api_code = 10120
-    result = None  # 结果可以是{} 或 []
+    api_code = 20024
+    data = None  # 结果可以是{} 或 []
 
 
 class FileExtensionException(ApiResponse):
     http_status_code = 401
     message = '文件扩展名不符合规范'
-    api_code = 10121
-    result = None  # 结果可以是{} 或 []
+    api_code = 20025
+    data = None  # 结果可以是{} 或 []
 
 
 class Success(ApiResponse):
     http_status_code = 200
     api_code = 20000
-    result = None  # 结果可以是{} 或 []
-    message = '自定义成功返回'
+    data = None  # 结果可以是{} 或 []
+    message = '请求成功'
     success = True
 
 
 class Fail(ApiResponse):
     http_status_code = 200
     api_code = 20110
-    result = None  # 结果可以是{} 或 []
-    message = '自定义成功返回'
+    data = None  # 结果可以是{} 或 []
+    message = '请求失败'
     success = False
